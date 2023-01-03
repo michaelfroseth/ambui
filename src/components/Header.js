60cb88004198f77516170e91e10203d570c99af6
Michael Froseth <info@michaelfroseth.com>
2023-01-03 13:45:27 -0600

    parallax

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,26 +2,29 @@ import React from "react";
 import HeroCTA from "./templates/HeroCTA";
 import IconBox from "./templates/IconBox";
 import Nav from "./templates/Nav";
+import { Parallax } from "react-parallax";
 
 const Header = () => {
   return (
-    <div className="home-hero">
-      <div className="container">
-        <header className="header">
-          <div className="logo">
-            <a href="/">
-              <img
-                src="./assets/ambition-logo-white.svg"
-                alt="Ambition Fire Protection"
-              />
-            </a>
-          </div>
-          <Nav />
-        </header>
-        <HeroCTA />
-        <IconBox />
+    <Parallax bgImage="./assets/dark-bg.jpg" strength={400}>
+      <div className="home-hero">
+        <div className="container">
+          <header className="header">
+            <div className="logo">
+              <a href="/">
+                <img
+                  src="./assets/ambition-logo-white.svg"
+                  alt="Ambition Fire Protection"
+                />
+              </a>
+            </div>
+            <Nav />
+          </header>
+          <HeroCTA />
+          <IconBox />
+        </div>
       </div>
-    </div>
+    </Parallax>
   );
 };
 
