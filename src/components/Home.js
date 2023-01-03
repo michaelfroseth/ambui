3f7d13f8086256038ab2077c19b59f8fa0203936
Michael Froseth <info@michaelfroseth.com>
2023-01-03 14:02:37 -0600

    css fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,55 +6,49 @@ function Home() {
   return (
     <div>
       <Header />
-      <Parallax
-        bgImage="./assets/intro-smoke.png"
-        strength={100}
-        bgImageStyle={{ backgroundPosition: "top" }}
-      >
-        <div className="dark-callout">
-          <div className="container">
-            <section className="intro">
-              <h2>Super SOAKING</h2>
-              <h3>Fire Extinguishers</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-                scelerisque lacinia diam eu eleifend. Phasellus nisi tortor,
-                aliquam nec fringilla a, mattis et lorem. Etiam id mattis sem,
-                vel convallis mauris.
-              </p>
-            </section>
-            <section className="left-intro">
-              <div className="column">
-                <div className="featured-img">
-                  <img src="./assets/featured-img-1.jpg" alt="Test" />
-                </div>
+      <div className="dark-callout">
+        <div className="container">
+          <section className="intro">
+            <h2>Super SOAKING</h2>
+            <h3>Fire Extinguishers</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              scelerisque lacinia diam eu eleifend. Phasellus nisi tortor,
+              aliquam nec fringilla a, mattis et lorem. Etiam id mattis sem, vel
+              convallis mauris.
+            </p>
+          </section>
+          <section className="left-intro">
+            <div className="column">
+              <div className="featured-img">
+                <img src="./assets/featured-img-1.jpg" alt="Test" />
               </div>
-              <div className="column">
-                <div className="intro-content">
-                  <span className="tagline">Protection You Can Count On</span>
-                  <h2>
-                    Keeping Your
-                    <br />
-                    Everything Safe
-                  </h2>
-                  <p>
-                    Duis sit amet venenatis nisl. Nam non rhoncus nisl, et
-                    suscipit sem. Vivamus condimentum lectus nec odio imperdiet
-                    tristique. Suspendisse nibh arcu, malesuada vel placerat sit
-                    amet, suscipit sit amet enim. Ut ac eleifend arcu.{" "}
-                  </p>
-                  <p>
-                    <strong>
-                      Morbi iaculis ligula vel est finibus, quis accumsan ligula
-                      suscipit.
-                    </strong>
-                  </p>
-                </div>
+            </div>
+            <div className="column">
+              <div className="intro-content">
+                <span className="tagline">Protection You Can Count On</span>
+                <h2>
+                  Keeping Your
+                  <br />
+                  Everything Safe
+                </h2>
+                <p>
+                  Duis sit amet venenatis nisl. Nam non rhoncus nisl, et
+                  suscipit sem. Vivamus condimentum lectus nec odio imperdiet
+                  tristique. Suspendisse nibh arcu, malesuada vel placerat sit
+                  amet, suscipit sit amet enim. Ut ac eleifend arcu.{" "}
+                </p>
+                <p>
+                  <strong>
+                    Morbi iaculis ligula vel est finibus, quis accumsan ligula
+                    suscipit.
+                  </strong>
+                </p>
               </div>
-            </section>
-          </div>
+            </div>
+          </section>
         </div>
-      </Parallax>
+      </div>
     </div>
   );
 }
