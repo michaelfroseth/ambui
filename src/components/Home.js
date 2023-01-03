e64e4cd7c07460491d889deb4c1953ed8cba9601
Michael Froseth <info@michaelfroseth.com>
2023-01-03 15:40:23 -0600

    Style Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,14 +36,50 @@ function Home() {
                   Duis sit amet venenatis nisl. Nam non rhoncus nisl, et
                   suscipit sem. Vivamus condimentum lectus nec odio imperdiet
                   tristique. Suspendisse nibh arcu, malesuada vel placerat sit
-                  amet, suscipit sit amet enim. Ut ac eleifend arcu.{" "}
+                  amet, suscipit sit amet enim. Ut ac eleifend arcu.
                 </p>
+                <br />
                 <p>
                   <strong>
                     Morbi iaculis ligula vel est finibus, quis accumsan ligula
                     suscipit.
                   </strong>
                 </p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+      <div className="white-cta"></div>
+      <div className="dark-callout-alt">
+        <div className="container">
+          <section className="right-intro">
+            <div className="column">
+              <div className="intro-content">
+                <span className="tagline">Protection You Can Count On</span>
+                <h2>
+                  Keeping Your
+                  <br />
+                  Everything Safe
+                </h2>
+                <p>
+                  Duis sit amet venenatis nisl. Nam non rhoncus nisl, et
+                  suscipit sem. Vivamus condimentum lectus nec odio imperdiet
+                  tristique. Suspendisse nibh arcu, malesuada vel placerat sit
+                  amet, suscipit sit amet enim. Ut ac eleifend arcu.
+                </p>
+                <br />
+                <p>
+                  <strong>
+                    Morbi iaculis ligula vel est finibus, quis accumsan ligula
+                    suscipit.
+                  </strong>
+                </p>
+              </div>
+            </div>
+            <div className="column">
+              <div className="featured-img">
+                <img src="./assets/featured-img-2.jpg" alt="Test" />
               </div>
             </div>
           </section>
